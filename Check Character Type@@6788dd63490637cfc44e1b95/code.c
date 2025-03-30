3edfd9d16fd9f7531ce4aa055b664a136cb58156
Abhinavpreet Singh Arora <aroraabhinavpreetsingh@gmail.com>
2025-03-30 14:22:04 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -2,20 +2,23 @@
 
 int main() {
     char a;
-    scanf("%c",&a);
-    int charvalue= (int) a;
-    if((charvalue>=65 && charvalue<=91 ) || (charvalue>=97 && charvalue<= 123)){
-        if(a=='a' || a=='e' || a=='i' || a=='o' || a=='u' || a=='A' || a=='E' || a=='I' || a=='O' || a=='U'){
+    scanf("%c", &a);
+    int charvalue = (int) a;
+
+    if ((charvalue >= 65 && charvalue <= 90) || (charvalue >= 97 && charvalue <= 122)) {
+        if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' ||
+            a == 'A' || a == 'E' || a == 'I' || a == 'O' || a == 'U') {
             printf("Vowel");
-        }else{
+        } else {
             printf("Consonant");
         }
-    }else{
-        if(a=='0' || a=='1' || a=='2' || a=='3' || a=='4' || a=='5' || a=='6' || a=='7' || a=='8' || a=='9'){
-            printf("Digit");
-        }else{
-            printf("Special Character");
-        }
+    } 
+    else if (a >= '0' && a <= '9') {
+        printf("Digit");
+    } 
+    else {
+        printf("Special Character");
     }
+
     return 0;
 }
